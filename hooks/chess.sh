35ebc6683f2c3ab009c340cd3e834762bbe261fc
Michael Bilenko <denso.ffff@gmail.com>
2016-06-15 11:21:23 +0300

    Fix scripts

--- hooks/chess.sh
+++ hooks/chess.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 OLDPORTS=( `docker ps | grep wriodockerproduction_chess_1 | awk '{print $1}'` )
-cd /wriodockerproduction
+cd /srv/docker/Wrio-Docker-Production
 docker pull webrunes/chess-wrio-game && docker-compose up -d chess
--- hooks/storage.sh
+++ hooks/storage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 OLDPORTS=( `docker ps | grep wriodockerproduction_storage_1 | awk '{print $1}'` )
-cd /wriodockerproduction
+cd /srv/docker/Wrio-Docker-Production
 docker pull webrunes/storage-wrio-app && docker-compose up -d storage
