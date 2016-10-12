d17dac0cbabb31a55990f751065654e1e5053b13
Michael Bilenko <denso.ffff@gmail.com>
2016-10-12 20:26:20 +0300

    Fix webgold auto deploy

--- hooks/webgold.sh
+++ hooks/webgold.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 OLDPORTS=( `docker ps | grep wriodockerproduction_webgold_1 | awk '{print $1}'` )
-cd /srv/docker/Wrio-Docker-Productiongg
+cd /srv/docker/Wrio-Docker-Production
 docker pull webrunes/webgold-wrio-app && docker-compose up -d webgold
