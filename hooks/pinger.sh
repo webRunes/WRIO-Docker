c6f0b23461fdbff1f24bfb3bed022540aff8ba27
Spirin Vladimir <spirin.vladimir@gmail.com>
2018-02-16 11:54:27 -0400

    rename "titter" to "pinger" for fix hook

--- hooks/pinger.sh
+++ hooks/pinger.sh
@@ -2,4 +2,4 @@
 
 OLDPORTS=( `docker ps | grep wriodockerproduction_titter_1 | awk '{print $1}'` )
 cd /srv/docker/Wrio-Docker-Production
-docker pull webrunes/titter-wrio-app && /usr/local/bin/docker-compose up -d pinger
+docker pull webrunes/pinger-wrio-app && /usr/local/bin/docker-compose up -d pinger
